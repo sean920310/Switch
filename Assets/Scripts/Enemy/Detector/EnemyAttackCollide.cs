d78ccff565716014ca9a012da5daaf6ec0820c7f
ArDai <78299618+sean920310@users.noreply.github.com>
2023-11-02 19:01:02 +0800

    Modify EnemyAttackCollide

--- Assets/Scripts/Enemy/Detector/EnemyAttackCollide.cs
+++ Assets/Scripts/Enemy/Detector/EnemyAttackCollide.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class EnemyAttackCollide : MonoBehaviour
 {
     [SerializeField] LayerMask playerLayer;
-    float _damage;
-    public void SetDamage(float damage) => _damage = damage;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if ((playerLayer & 1 << collision.gameObject.layer) == 1 << collision.gameObject.layer)
         {
             Debug.Log("Player Hurt: " + collision.gameObject.name);
-            collision.gameObject.GetComponent<EntityBase>().GetDamage(gameObject.GetComponentInParent<EntityBase>(), (int)_damage);
+            gameObject.GetComponentInParent<EntityBase>().SetDamage(collision.gameObject.GetComponent<EntityBase>());
             this.enabled = false;
         }
     }
