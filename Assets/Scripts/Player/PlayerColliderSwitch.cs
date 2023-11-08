e1738a52cb93ae1fc735c445ebe3fbd143587a9d
RinRainbow <b11015017@gapps.ntust.edu.tw>
2023-11-09 06:09:07 +0800

    Camera Setting
    3D camera rotation.x & CamTarget setting
    -> script in RinColliderSwutch.cs

--- Assets/Scripts/Player/PlayerColliderSwitch.cs
+++ Assets/Scripts/Player/PlayerColliderSwitch.cs
@@ -7,7 +7,13 @@ public class PlayerColliderSwitch : MonoBehaviour
     [SerializeField]
     public float colliderSize3D_x = 0.3779367f;
     [SerializeField]
-    public float colliderSize3D_y = 0.7923383f;
+    public float colliderSize3D_y = 0.69f;
+    [SerializeField]
+    public float colliderOffset3D_x = 0.1072523f;
+    [SerializeField]
+    public float colliderOffset3D_y = -0.72f;
+    [SerializeField]
+    private GameObject camTarget;
 
     // Update is called once per frame
     private void LateUpdate()
@@ -15,10 +21,14 @@ public class PlayerColliderSwitch : MonoBehaviour
         if (CameraManager.Instance.DimensionState == CameraManager.Dimension.ThreeD)
         {
             GetComponent<BoxCollider2D>().size = new Vector2(colliderSize3D_x, colliderSize3D_y);
+            GetComponent<BoxCollider2D>().offset = new Vector2(colliderOffset3D_x, colliderOffset3D_y);
+            camTarget.transform.localPosition = new Vector3(0f, 1.403f, -0.6f);
         }
         else
         {
             GetComponent<BoxCollider2D>().size = new Vector2(0.3779367f, 1.591878f);
+            GetComponent<BoxCollider2D>().offset = new Vector2(0.1072523f, 0.005540371f);
+            camTarget.transform.localPosition = new Vector3(0f, 1.403f, 0f);
         }
     }
 }
