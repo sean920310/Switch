006f130bdd4377e4aa0b7d6928afae14a6232b0f
Winter <63649186+winter901017@users.noreply.github.com>
2023-11-27 23:55:12 +0800

    Add: Using skill counter

--- Assets/Scripts/UI/Skill.cs
+++ Assets/Scripts/UI/Skill.cs
@@ -9,9 +9,14 @@ public class Skill : MonoBehaviour
     public float pressScale = 0.5F;
     public float duration = 1F;
     private bool isAnim = false;
+    private bool isSkillUsing = false;
 
+
+    public GameObject skillMask;
     public Image UIobj;
-    public float countTime = 5.0f;
+    public Image UsingObj;
+    public float cdTime = 5.0f;
+    public float usingTime = 5.0f;
     [SerializeField, ReadOnly] float remainTime = 0;
     public Text time;
 
@@ -24,31 +29,65 @@ public class Skill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (UsingObj.fillAmount != 0)
+        {
+            skillMask.SetActive(true);
+            UsingObj.fillAmount -= 1.0f / usingTime * Time.deltaTime;
+            remainTime -= Time.deltaTime;
+        }
+
         if (UIobj.fillAmount != 0)
         {
-            UIobj.fillAmount -= 1.0f / countTime * Time.deltaTime;
+            skillMask.SetActive(true);
+            UIobj.fillAmount -= 1.0f / cdTime * Time.deltaTime;
             remainTime -= Time.deltaTime;
-            time.text = ((int)remainTime).ToString();
+            if (remainTime >= 1)
+            {
+                time.text = ((int)remainTime).ToString();
+            }
+            else
+            {
+                time.text = remainTime.ToString("0.00");
+            }
         }
         if (remainTime < 0)
         {
-            remainTime = 0;
-            time.text = "0";
-            time.enabled = false;
+            if (isSkillUsing)
+            {
+                UIobj.fillAmount = 1.0f;
+                time.enabled = false;
+                remainTime = usingTime;
+                time.enabled = true;
+                isSkillUsing = false;
+            }
+            else
+            {
+                remainTime = 0;
+                time.text = "";
+                time.enabled = false;
+                isSkillUsing = false;
+                skillMask.SetActive(false);
+            }
         }
+    }
+
+    public bool IsUsingSkillState()
+    {
+        return isAnim || isSkillUsing;
     }
 
     public async void PressKeyBoard()
     {
-        UIobj.fillAmount = 1.0f;
-        remainTime = countTime;
-        time.enabled = true;
-
-        if (isAnim)
+        if (IsUsingSkillState())
         {
             return;
         }
         isAnim = true;
+        isSkillUsing = true;
+        UsingObj.fillAmount = 1.0f;
+        remainTime = usingTime;
+        time.text = "";
+        time.enabled = true;
         Debug.Log("PressKeyBoard");
         transform.DOScale(new Vector3(1 - pressScale, 1 - pressScale, 1 - pressScale), duration);
         await Task.Delay((int)(duration * 1000));
