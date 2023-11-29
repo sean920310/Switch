6ef3571f947578366d4ce23977622e9f4db6c323
Rin Kawano <b11015017@gapps.ntust.edu.tw>
2023-11-29 18:42:17 +0800

    Mortar delay time
    Added delay time, so that trap can act  more flexible.

--- Assets/Scripts/Trap/ArrowShot.cs
+++ Assets/Scripts/Trap/ArrowShot.cs
@@ -7,23 +7,34 @@ public class ArrowShot : MonoBehaviour
     [SerializeField] GameObject arrow;
     [SerializeField] float arrowSpeed = 10.0f;
     [SerializeField] float setWaittingTime = 1.0f;
+    [SerializeField] float setDelayTime = 1.0f;
     [SerializeField, ReadOnly] float countDown;
+    [SerializeField, ReadOnly] float delay;
     [SerializeField] float suicideTime = 1.0f;
     public Transform shotPoint;
 
     private void Start()
     {
         countDown = setWaittingTime;
+        delay = setDelayTime;
     }
 
     void Update()
     {
-        countDown -= Time.deltaTime;
-        if (countDown <= 0)
+        if(delay > 0)
         {
-            Shot();
-            countDown = setWaittingTime;
+            delay -= Time.deltaTime;
         }
+        else
+        {
+            countDown -= Time.deltaTime;
+            if (countDown <= 0)
+            {
+                Shot();
+                countDown = setWaittingTime;
+            }
+        }
+        
     }
 
     void Shot()
