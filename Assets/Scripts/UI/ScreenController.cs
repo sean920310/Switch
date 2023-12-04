9af2adc6d7a347daf84a0e2f9d40c968bc7afd47
Rin Kawano <b11015017@gapps.ntust.edu.tw>
2023-12-04 22:15:37 +0800

    danger effect finished

--- Assets/Scripts/UI/ScreenController.cs
+++ Assets/Scripts/UI/ScreenController.cs
@@ -9,11 +9,18 @@ public class ScreenController : MonoBehaviour
     public GameObject GameOverPanel;
     public GameObject DangerPanel;
     Color GameOver_color;
+    Color Danger_color;
 
     [SerializeField] Button GameOverButton;
     [SerializeField] Button DangerButton;
 
+    [SerializeField] float danger_speed = 2f;
+    [SerializeField] float danger_min = 0f;
+    [SerializeField] float danger_max = 1f;
+
     bool GameOverReady = false;
+    bool DangerReady = false;
+    bool isUp = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +31,10 @@ public class ScreenController : MonoBehaviour
         GameOver_color = GameOverPanel.GetComponent<Image>().color;
         GameOver_color.a = 0f;
         GameOverPanel.GetComponent<Image>().color = GameOver_color;
+
+        Danger_color = DangerPanel.GetComponent<Image>().color;
+        Danger_color.a = danger_min;
+        DangerPanel.GetComponent<Image>().color = Danger_color;
     }
 
     // Update is called once per frame
@@ -36,6 +47,33 @@ public class ScreenController : MonoBehaviour
             GameOverPanel.GetComponent<Image>().color = GameOver_color;
         }
         else GameOverReady = false;
+
+        if(DangerReady)
+        {
+            Danger_color = DangerPanel.GetComponent<Image>().color;
+            if (Danger_color.a < danger_max && isUp)
+            {
+                Danger_color.a += Time.deltaTime * danger_speed;
+                DangerPanel.GetComponent<Image>().color = Danger_color;
+            }
+            else if (Danger_color.a >= danger_max)
+            {
+                isUp = false;
+                Danger_color.a -= Time.deltaTime * danger_speed;
+                DangerPanel.GetComponent<Image>().color = Danger_color;
+            }
+            else if (Danger_color.a <= danger_min)
+            {
+                isUp = true;
+                Danger_color.a += Time.deltaTime * danger_speed;
+                DangerPanel.GetComponent<Image>().color = Danger_color;
+            }
+            else if (danger_min < Danger_color.a && !isUp)
+            {
+                Danger_color.a -= Time.deltaTime * danger_speed;
+                DangerPanel.GetComponent<Image>().color = Danger_color;
+            }
+        }
     }
 
     public void onClickGameOverButton()
@@ -48,11 +86,20 @@ public class ScreenController : MonoBehaviour
         GameOverPanel.GetComponent<Image>().color = GameOver_color;
 
         GameOverReady = true;
+        DangerReady = false;
     }
 
     public void onClickDangerButton()
     {
         GameOverPanel.SetActive(false);
         DangerPanel.SetActive(true);
+
+        Danger_color = DangerPanel.GetComponent<Image>().color;
+        Danger_color.a = danger_min;
+        isUp = true;
+        DangerPanel.GetComponent<Image>().color = Danger_color;
+
+        GameOverReady = false;
+        DangerReady = true;
     }
 }
