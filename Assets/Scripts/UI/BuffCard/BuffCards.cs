414b54a6b62ca93f4362125c31181be1b8744148
ken5170696 <96158757+ken5170696@users.noreply.github.com>
2023-12-11 21:38:31 +0800

    Buff Card 串接完成

--- Assets/Scripts/UI/BuffCard/BuffCards.cs
+++ Assets/Scripts/UI/BuffCard/BuffCards.cs
@@ -6,10 +6,20 @@ using UnityEngine;
 
 public class BuffCards : MonoBehaviour
 {
+    public enum BuffIcon
+    {
+        SWORD,
+        SHEILD
+    }
+
     public Animator anim;
     public BuffCard[] cards;
     public Sprite[] cardBG;
     public Sprite[] cardIcon;
+
+    public delegate void OnBuffClicked(int cardIdx);
+    public event OnBuffClicked onBuffClicked;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,34 +28,34 @@ public class BuffCards : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Debug.Log("Card_Show");
-            anim.Play("Card_Show");
-            SetBuffCard(0, 1);
-            SetBuffCard(1, 0);
-            SetBuffCard(2, 1);
-        }
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    Debug.Log("Card_Show");
+        //    anim.Play("Card_Show");
+        //    SetBuffCard(0, 1);
+        //    SetBuffCard(1, 0);
+        //    SetBuffCard(2, 1);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            Debug.Log("Card_Hide");
-            anim.Play("Card_Hide");
-        }
+        //if (Input.GetKeyDown(KeyCode.V))
+        //{
+        //    Debug.Log("Card_Hide");
+        //    anim.Play("Card_Hide");
+        //}
     }
 
     /*
     cardIdx: 0~2
     type: 0是劍 1是盾
     */
-    public void SetBuffCard(int cardIdx, int type/*, BuffSystem.Model.BuffSO buffSO = null*/)
+    public void SetBuffCard(int cardIdx, string descriptions, BuffIcon type/*, BuffSystem.Model.BuffSO buffSO = null*/)
     {
         // cardIdx.ToString() 需替換成文字內容
-        cards[cardIdx].SetBuffCard(cardIdx.ToString(), cardBG[type], cardIcon[type]);
+        cards[cardIdx].SetBuffCard(descriptions, cardBG[((int)type)], cardIcon[((int)type)]);
     }
 
     public void BuffCardClick(int cardIdx)
     {
-        Debug.Log("BuffCard_Clicked: " + cardIdx);
+        onBuffClicked?.Invoke(cardIdx);
     }
 }
