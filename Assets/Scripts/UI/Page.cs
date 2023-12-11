624a228e4f00921baabf89cacb70e0f156b5bedd
ArDai <sean920310@gmail.com>
2023-12-11 21:57:37 +0800

    Save Build setting

--- Assets/Scripts/UI/Page.cs
+++ Assets/Scripts/UI/Page.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DG.Tweening;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.UI;
 
